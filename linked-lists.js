18c8457211642110056391b3112a24e45f5f1996
Ben Flores <benjaminhyw@gmail.com>
2017-10-03 22:43:17 -0700

    Add if/else block in .addToHead that updates LinkedList node properties accordingly

--- linked-lists.js
+++ linked-lists.js
@@ -18,4 +18,15 @@ LinkedList.prototype.addToHead = function(value){
   // Make the next prop the current linkedList head,
   // and make prev null, because there shouldn't be any previous nodes to the head
   var newNode = new Node(value, this.head, null);
+
+  if (this.head){
+    //If there is at least one node (meaning, there will also already be an existing head)
+    this.head.prev = newNode;
+  } else {
+    //Otherwise if there aren't any nodes, make this newNode the tails
+    this.tail = newNode;
+  }
+  //This is outside of the if/else block because regardless of the outcome, this needs to happen (when using addToHead)
+  this.head = newNode;
 };
+
