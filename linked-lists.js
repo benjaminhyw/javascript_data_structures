b60aa3690902c8fe1a07ed6c1f570fbe469ea8d9
Ben Flores <benjaminhyw@gmail.com>
2017-10-04 11:34:10 -0700

    Add .search() functionality

--- linked-lists.js
+++ linked-lists.js
@@ -70,3 +70,18 @@ LinkedList.prototype.removeTail = function(){
   }
   return val;
 };
+
+LinkedList.prototype.search = function(searchValue){
+  // We set currentNode to this.head; to start off, since we have to start from either the head or tail.  To make this function work, we have to go through the entire linked list and see if the searchValue exists inside the linked list
+  var currentNode = this.head;
+
+  // This while loop will continue to run, and will only stop if currentNode = null.  In which case, the loop closes and we return null at the very end (indicating searchValue was not found);
+  while (currentNode){
+    if (currentNode.value === searchValue){
+      return currentNode.value;
+    }
+    currentNode = currentNode.next;
+  }
+  return null;
+
+};
