d100eb0bae0e5708afc7e11db8619302a68c7c26
Ben Flores <benjaminhyw@gmail.com>
2017-10-04 15:27:20 -0700

    Create binary-search-trees.js, add first BST constructor function

---
+++ binary-search-trees.js
@@ -0,0 +1,9 @@
+// Binary Search Tree
+
+// Constructor function
+// 3 properties: value, left child and right child
+function BST(value){
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
