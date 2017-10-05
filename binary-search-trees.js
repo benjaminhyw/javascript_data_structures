058aff68c2edd66289a483ad2a3f4560c7fa4f7c
Ben Flores <benjaminhyw@gmail.com>
2017-10-05 13:46:01 -0700

    Add if statement that allows for pre-order depth traversal.  Also included lengthy comment to help better understand

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -56,19 +56,30 @@ BST.prototype.contains = function(value){
 // This function takes another function as a parameter
 // This method follows every branch down to the bottom, and running our iterator function on every node all the way down to the bottom before we move on to our next branch
 BST.prototype.depthFirstTraversal = function(iteratorFunc, order){
+  // This allows for pre-order traversal, which goes from top to bottom left, and then move towards the right
+  // It touches the parent node first, then goes down the left branch, then goes down the right branch
+  if (order === 'pre-order'){
+    iteratorFunc(this.value)
+  }
   // If there's a left node, recursively run this function on the left node
   if (this.left){
     this.left.depthFirstTraversal(iteratorFunc, order);
   }
   if (order === 'in-order'){
     // Then run this on the parent node
+    // This will return nodes from least to greatest
+    // Touches left node first, then the parent node, then the right node
     iteratorFunc(this.value);
   }
-
-
   // Then run this on the right child nodes
   if (this.right){
     this.right.depthFirstTraversal(iteratorFunc, order);
   }
   // You end up doing this for all branches of the tree
 };
+
+/*The way the previous function works relies on the placement of the if statements, and whatever is passed as the order parameter.  If you notice, the 'pre-order' option is first, which says run it on this.value, aka the parent node.  Then it goes to the rest of the block, making it move left and then right.
+
+
+Otherwise if the order is 'in-order' instead of 'pre-order', it ignores the first statement, making it start on the left, then up, then parent, then right*/
+
